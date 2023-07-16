802afb527c0ddd5f8f1610af8a69e114ca9c1a13
DanPeled <danpeled31@gmail.com>
2023-07-16 10:53:08 +0300

    changed world bg, fixed bug in main menu

--- Assets/Scripts/MainMenu/MainMenuSelect.cs
+++ Assets/Scripts/MainMenu/MainMenuSelect.cs
@@ -5,21 +5,24 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Linq;
+
 public class MainMenuSelect : MonoBehaviour
 {
     public int currentAction = 0;
     public LevelLoader levelLoader;
     public GameObject[] buttons;
     public bool isMobile;
-    public GameObject playMenu, normalMenu;
+    public GameObject playMenu,
+        normalMenu;
+
     public void SetMobile()
     {
         MobileControls.isMobilePersisted = !isMobile;
         isMobile = MobileControls.isMobilePersisted;
     }
+
     void Update()
     {
-
         // Action change
         if (InputSystem.down.isClicked())
         {
@@ -46,35 +49,40 @@ public class MainMenuSelect : MonoBehaviour
                 case 2:
                     Quit();
                     break;
-
             }
         }
         for (int i = 0; i < buttons.Length; i++)
         {
-            buttons[i].GetComponentInChildren<TextMeshProUGUI>().color = i == currentAction ? GlobalSettings.i.highlightedColor : Color.black;
+            buttons[i].GetComponentInChildren<TextMeshProUGUI>().color =
+                i == currentAction ? GlobalSettings.i.highlightedColor : Color.black;
         }
     }
+
     public void PlayMenu()
     {
-        playMenu.SetActive(true);
-        normalMenu.SetActive(false);
+        // playMenu.SetActive(true);
+        // normalMenu.SetActive(false);
+        Play();
     }
+
     public void Play()
     {
         levelLoader.Load("World");
     }
+
     public void WorldEditor()
     {
         SceneManager.LoadScene("WorldEditor");
     }
+
     public void Settings()
     {
         // TODO: implement settings function
     }
+
     public void Quit()
     {
         Debug.Log("Quit");
         Application.Quit();
     }
-
 }
